53c396acaa936968e3ae45de9d32e5e8aac4e7f9
Kelly M Collins <90kcol@gmail.com>
2019-12-04 11:10:29 -0500

    Added top nav and added input to change player name with submit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('I Work')
+//Change the player name upon click
+let form = document.querySelector('#form');
+const formInput = form.querySelector('input.pName');
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  document.querySelector('#playerName').innerHTML = formInput.value
+})
