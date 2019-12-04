667386cb637526efe60990c220a71c88f01f660e
Kelly M Collins <90kcol@gmail.com>
2019-12-04 14:31:38 -0500

    Add class for video game questions music questions

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ form.addEventListener('submit', evt => {
   document.querySelector('form').style.display = 'none';
   document.querySelector('.chooseCategory').style.display = 'flex';
 })
+//Create a class for the quiz and the questions
+class Quiz {
+  constructor(questions) {
+    this.questions = questions
+    this.score = 0
+  }
+}
+
+class Question {
+  constructor(questions, answers, correctAnswer) {
+    this.questions = questions
+    this.answers = answers
+    this.correctAnswer = correctAnswer
+  }
+}
+
+const vgQuestions = new Question
+console.log(vgQuestions)
+const musicQuestions = new Question
+console.log(musicQuestions)
